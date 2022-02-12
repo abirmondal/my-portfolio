e711056d87b44b36e44af66ba5fbc5dc5ad375f9
Abir Mondal <abir22197072@gmail.com>
2022-02-12 09:07:24 +0530

    scroll to top button

--- js/main.js
+++ js/main.js
@@ -10,4 +10,30 @@ $(document).ready(function(){
             typingFun();
         }
     }, eachInterval);
+
+    $(document).scroll(function () {
+        var scroll_pos = $(this).scrollTop();
+
+        // FOR SCROLL TOP BUTTON
+        if (scroll_pos > 200) {
+            if ($('.scrollTop').css('bottom') == '-50px') {
+                $('.scrollTop').animate({ bottom: "50px" });
+            }
+        } else {
+            if ($('.scrollTop').css('bottom') == '50px') {
+                $('.scrollTop').animate({ bottom: "-50px" });
+            }
+        }
+    });
+
+    // SCROLL TOP BUTTON CLICK
+    $('.scrollTop').click(function () {
+        topScroll();
+    })
 });
+
+
+// SCROLL TO TOP
+function topScroll() {
+    $(window).scrollTop(0);
+}
